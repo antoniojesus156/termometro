9af51ccdb7d908a6d67b2d6237425a22ad1b09c1
Antonio Jesús <44335139+antoniojesus156@users.noreply.github.com>
2022-02-21 12:41:49 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(emperatura_maxima)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(temperatura_minima)
+})
+let temperatura_minima = 0
+let emperatura_maxima = 0
+let temperatura_actual = 0
+temperatura_actual += input.temperature()
+emperatura_maxima += temperatura_actual
+temperatura_minima += temperatura_actual
+basic.forever(function () {
+    temperatura_actual += input.temperature()
+    if (emperatura_maxima < temperatura_actual) {
+        emperatura_maxima = temperatura_actual
+    }
+    if (temperatura_minima > temperatura_actual) {
+        temperatura_minima = temperatura_actual
+    }
+})
